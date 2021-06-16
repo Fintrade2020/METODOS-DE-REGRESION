7faa5b683b9dec05322e32594feaeece9763e652
Laboratorio Fintrade <60900100+Fintrade2020@users.noreply.github.com>
2021-06-16 12:57:56 -0500

    Add files via upload

---
+++ happymodel.R
@@ -0,0 +1,54 @@
+#En las próximas lineas se desarrolla una rutina de regresión a partir de una base de datos llamada "happy" que podemos encontrar en la libreria "faraway" 
+#ANÁLISIS PRELIMINAR
+#De la librería "faraway" llamamos base de datos "happy" (en ayuda puedes visualizar en qué consiste la base)
+library(faraway)
+data(happy)
+help(happy)
+#Presente la correlación y los gráficos de dispersión entre todas las variables.
+attach(happy)
+cor(happy)
+plot(happy)
+#REGRESIÓN LINEAL MÚLTIPLE
+#Estime un modelo de regresión con todas las variables tomando "happy" como variable dependiente. Muestre los principales resultados. 
+modelo1<-lm(happy$happy~money+sex+love+work)    
+summary(modelo1)
+#Calcule los intervalos de confianza de los estimadores.
+confint(modelo1)
+#Presente la tabla "anova".
+anova(modelo1)
+#Estime un modelo con las variables que fueron significativas individualmente al 10%. 
+modelo2<-lm(happy$happy~money+love+work)    
+summary(modelo2)
+#Estime un modelo con las variables que fueron significativas al 5%. 
+modelo3<-lm(happy$happy~love+work)    
+summary(modelo3)
+#REGRESIÓN CON VARIABLES DUMMY
+#Observe que la variable "love" es importante, sin embargo, se ha tomado como si fuera cuantitativa. Reconstruya esta variable mediante dos dummy y estime de nuevo el modelo donde las otras independientes serán "money" y "work". 
+love2<-as.numeric(love==2)
+love3 <- as.numeric(love==3)
+modelo4<-lm(happy$happy~money+love2+love3+work)    
+summary(modelo4)
+#Ahora estime un modelo log lin.
+modelo5<-lm(log(happy$happy)~money+love2+love3+work)    
+summary(modelo5)
+#SELECCIÓN DEL MODELO
+#Calcule los criterios AIC y BIC para los modelos tres primeros modelos. Determine el mejor modelo.
+library(stats4)
+AIC(modelo1,modelo2,modelo3)
+BIC(modelo1,modelo2,modelo3)
+#Utilice el metodo secuencial backward para obtener el mejor modelo
+step(modelo1)
+#VARIABLE DEPENDIENTE CUALITATIVA
+#Modifiquemos el análisis y supongamos que tenemos sex como dependiente.
+modlog1<-glm(sex~money+love+work,family="binomial")
+summary(modlog1)
+step(modlog1)
+modlog2<-glm(sex~money+work,family="binomial")
+summary(modlog2)
+#Interpretemos los resultados primero calculando el pronostico y luego la probabilidad.
+logratio<-predict(modlog2)
+ratio<-exp(logratio)
+piest<-ratio/(1+ratio)
+plot(piest)
+#La interpretación depende de los valores x. Por lo tanto podemos calcular el efecto marginal a partir del promedio de la probabilidad.
+modlog2$coefficients*mean(piest)*(1-mean(piest))library(faraway)
